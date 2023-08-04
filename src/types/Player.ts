8940e200a5079cc0b099f10702270084d8f70d81
JoaoGuiMB <juaogui159@gmail.com>
2023-08-04 10:22:38 -0300

    feat: player type

--- src/types/Player.ts
+++ src/types/Player.ts
@@ -1,4 +1,18 @@
+export interface IAvatar {
+  eyeType: string;
+  accessoriesType: string;
+  topType: string;
+  hairColor: string;
+  facialHairType: string;
+  clotheType: string;
+  eyebrowType: string;
+  mouthType: string;
+  skinColor: string;
+}
+
 export interface Player {
+  id: string;
+  points: number;
   nickName: string;
-  avatarId: string;
+  avatar: IAvatar;
 }
